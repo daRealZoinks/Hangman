6c9a7bb9667c855ace879b006050cfa6727e7da2
Zoinks <murarashuvlad@gmail.com>
2022-07-15 17:56:21 +0300

    testing out the github copilot extention

--- Hangman/Hangman.cpp
+++ Hangman/Hangman.cpp
@@ -1,6 +1,121 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main()
+using namespace std;
+
+string pickAWord()
 {
-	std::cout << "Hello World!\n";
+	vector<string> possibleWords = {
+		"apple",
+		"banana",
+		"orange",
+		"coconut",
+		"strawberry",
+		"lime",
+		"grapefruit",
+		"watermelon",
+		"mango",
+		"cherry",
+		"peach",
+		"pear",
+		"kiwi",
+		"avocado",
+		"pineapple",
+		"blueberry",
+		"blackberry",
+		"raspberry",
+		"gooseberry",
+		"blackcurrant",
+		"cranberry",
+		"clementine",
+		"fig",
+		"date",
+		"elderberry",
+		"ginkgo",
+		"grape",
+		"guava",
+		"jackfruit",
+		"jambul",
+		"jicama",
+		"kiwano",
+		"kumquat",
+		"lemon",
+		"lime",
+		"loquat",
+		"lychee",
+		"mango",
+		"melon",
+		"nectarine",
+		"nut",
+		"olive",
+		"orange",
+		"papaya",
+		"passionfruit",
+		"peach",
+		"pear",
+		"persimmon",
+		"physalis",
+		"plum",
+		"pomegranate",
+		"pomelo",
+		"quince",
+		"raspberry",
+		"rambutan",
+		"ramie",
+		"raspberries",
+		"rhubarb",
+		"rock melon",
+		"salal",
+		"salmonberry",
+		"satsuma",
+		"soursop",
+		"star fruit",
+		"strawberry",
+		"tamarillo",
+		"tangerine",
+		"tomato",
+	}
+
+	int randomIndex = rand() % possibleWords.size();
+	return possibleWords[randomIndex];
+}
+
+main()
+{
+	auto wordToGuess = pickAWord();
+
+	auto guessedLetters = vector<char>();
+
+	auto guessesRemaining = 10;
+
+	while(guessesRemaining > 0)
+	{
+		auto letter = ' ';
+		cout << "Guess a letter: ";
+		cin >> letter;
+		guessedLetters.push_back(letter);
+		cout << "You guessed: ";
+		for(auto letter : guessedLetters)
+		{
+			cout << letter << " ";
+		}
+		cout << endl;
+		cout << "You have " << guessesRemaining << " guesses remaining." << endl;
+		cout << "The word to guess is: " << wordToGuess << endl;
+		cout << "The word to guess is: ";
+		for(auto letter : wordToGuess)
+		{
+			if(find(guessedLetters.begin(), guessedLetters.end(), letter) != guessedLetters.end())
+			{
+				cout << letter << " ";
+			}
+			else
+			{
+				cout << "_ ";
+			}
+		}
+		cout << endl;
+		guessesRemaining--;
+	}
 }
